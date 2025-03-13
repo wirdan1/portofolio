da2f3c1eafa79ada3e5c7aca4728496520268022
wirdan1 <danz124c@gmail.com>
2025-03-14 00:11:22 +0700

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Website Loaded");
-});
+// Toggle Menu
+function toggleMenu() {
+    const menu = document.getElementById("menu");
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
+}
+
+// Pindah Halaman
+function showSection(sectionId) {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("profile").style.display = "none";
+    document.getElementById("tiktok").style.display = "none";
+    document.getElementById(sectionId).style.display = "block";
+}
